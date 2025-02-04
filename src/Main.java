1177b4cf1f006d355c4f822742b25be90dd7b1b0
Sakura <sakurathetrans@gmail.com>
2025-02-04 14:30:58 -0500

    Added functions this time

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,49 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<String> tasks = new ArrayList<>();
+    static void add(){
+        System.out.println("Enter a description of the new task.");
+        tasks.add(input.nextLine());
+    }
+
+    static void remove(){
+        System.out.println("Enter the index of the task to remove.");
+        tasks.remove(input.nextInt());
+    }
+
+    static void update(){
+        System.out.println("Enter the index of the task to update.");
+        int selectedTask = input.nextInt();
+        input.nextLine();
+        System.out.println("Enter the new description of the task.");
+        tasks.set(selectedTask, input.nextLine());
+    }
+
+    static void list(){
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + ". " + tasks.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<String>();
-        int selectedTask = 0;
         int userInput = 0;
         while(userInput != 5){
             System.out.println("Please choose an option:\n(1) Add a task.\n(2) Remove a task.\n(3) Update a task.\n(4) List all tasks.\n(0) Exit.)");
             userInput = input.nextInt();
             input.nextLine();
             if (userInput == 1){
-                System.out.println("Enter a description of the new task.");
-                tasks.add(input.nextLine());
+                add();
             }
             if (userInput == 2){
-                System.out.println("Enter the index of the task to remove.");
-                tasks.remove(input.nextInt());
+                remove();
             }
             if (userInput == 3){
-                System.out.println("Enter the index of the task to update.");
-                selectedTask = input.nextInt();
-                input.nextLine();
-                System.out.println("Enter the new description of the task.");
-                tasks.set(selectedTask, input.nextLine());
+                update();
             }
             if (userInput == 4){
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i));
-                }
+                list();
             }
             if (userInput == 0){
                 break;
